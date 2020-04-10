0017a8f029b94fa626a57c1f2550973138d6e13a
Kartik Goel <goel.kartik39@gmail.com>
2020-04-11 01:18:28 +0530

    Middleware to verify jwt

---
+++ middleware/requireLogin.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../config/keys").JWT_SECRET
+const mongoose = require("mongoose");
+const User = require("../models/users");
+
+module.exports = (req,res,next)=> {
+    const {authorization} = req.headers;
+    if(!authorization) {
+        return res.status(401).json({
+            error : "no header provided"
+        })
+    }
+    const token = authorization.replace("Bearer ", "");
+    jwt.verify(token,secret,(err,payload)=>{
+        if(err) {
+            return res.status(401).json({
+                err : "You must be logged ins"
+            })
+        }
+        const {_id} = payload;
+        User.findById(_id).then((userdata) => {
+            req.user = userdata
+        })
+        next();
+    })
+}
