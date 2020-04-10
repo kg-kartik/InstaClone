c9735e8b092e6383edc7c0758ad87ef136a567a1
Kartik Goel <goel.kartik39@gmail.com>
2020-04-11 01:18:44 +0530

    Updated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const db = require('./config/keys').MongoURI;
+const authroute = require("./routes/auth");
 
 mongoose.connect(db, {useNewUrlParser : true,
     useUnifiedTopology: true,
@@ -17,6 +18,8 @@ app.use(express.urlencoded({
 }));
 
 app.use(bodyParser.json());
+
+app.use("/",authroute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on ${PORT}`));
