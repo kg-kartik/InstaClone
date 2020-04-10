bc5db7c8658002c2aef4673c77415581c73ac2b5
Kartik Goel <goel.kartik39@gmail.com>
2020-04-10 15:53:41 +0530

    initialized server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const db = require('./config/keys').MongoURI;
+
+mongoose.connect(db, {useNewUrlParser : true,
+    useUnifiedTopology: true,
+    useCreateIndex : true})
+.then(() => console.log('Database connected'))
+.catch((err) => {
+    console.log(err);
+})
+
+app.use(express.urlencoded({
+    extended : false
+}));
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on ${PORT}`));
