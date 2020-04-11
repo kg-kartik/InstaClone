37503eeb6bb09205cefdc210e6a97f56e6dde50e
Kartik Goel <goel.kartik39@gmail.com>
2020-04-11 12:21:09 +0530

    Made post model

---
+++ models/post.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const {ObjectId} = mongoose.Schema.Types
+
+const postSchema = new Schema({
+    title : {
+        type : String,
+        require : true
+    },
+    body : {
+        type : String,
+        required : true
+    },
+    photo : {
+        type : String,
+        default : "No photo"
+    },
+    postedBy : {
+        type :ObjectId,
+        ref : "userModel"
+    }
+})
+
+const postModel = mongoose.model("postModel",postSchema);
+module.exports = postModel;
