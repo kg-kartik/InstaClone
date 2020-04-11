9e7de844c0f817e3a370eeedba47027ed7859944
Kartik Goel <goel.kartik39@gmail.com>
2020-04-12 00:30:42 +0530

    Added react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./screens/Home"
+import SignUp from "./screens/SignUp"
+import SignIn from "./screens/SignIn"
+import Profile from "./screens/Profile"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Route path exact="/">
+        <Home />
+      </Route>
+
+      <Route path ="/signup">
+        <SignUp />
+      </Route>
+
+      <Route path ="/signin">
+        <SignIn />
+      </Route>
+      
+      <Route path ="/profile">
+        <Profile />
+      </Route>
+
+    </BrowserRouter>
   );
 }
 
