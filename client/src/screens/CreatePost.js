d67c5f1d310c225cd1262dcd33743247564cbe85
Kartik Goel <goel.kartik39@gmail.com>
2020-04-12 01:05:21 +0530

    Made UI for createpost page

---
+++ client/src/screens/CreatePost.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const CreatePost = () => {
+    return (
+        <div className="card input-field">
+            <input type="text" placeholder="title" />
+            <input type="text" placeholder="body" />
+            <div className="file-field input-field">
+                <div className="btn">
+                    <span>File</span>
+                    <input type="file" />
+                </div>
+                <div className="file-path-wrapper">
+                    <input className="file-path validate" type="text" />
+                </div>
+            </div>
+            <button className="btn waves-effect waves-light">
+                    Submit Post
+            </button>
+        </div>
+    )
+}
+
+export default CreatePost
