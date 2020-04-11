b7aa8f2deeac14313d259abacd6ccefcc3916ddb
Kartik Goel <goel.kartik39@gmail.com>
2020-04-12 00:29:03 +0530

    added navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "../App.css"
+
+const Navbar = () => {
+    return (
+    <nav>
+        <div className="nav-wrapper white">
+            <a href="#" className="brand-logo left">Instagram</a>
+            <ul id="nav-mobile" className="right">
+            <li><a href="/signup">SignUp</a></li>
+            <li><a href="/signin">SignIn</a></li>
+            <li><a href="/profile">Profile</a></li>
+            </ul>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
