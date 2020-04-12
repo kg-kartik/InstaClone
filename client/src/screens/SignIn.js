2fb8c19cfb7a66fac9459927e930d54d754eaef4
Kartik Goel <goel.kartik39@gmail.com>
2020-04-13 02:15:50 +0530

    Connected to backend

--- client/src/screens/SignIn.js
+++ client/src/screens/SignIn.js
@@ -1,20 +1,57 @@
-import React from "react"
-import {Link,Router} from "react-router-dom"
+import React,{useState} from "react"
+import {Link,Router,useHistory} from "react-router-dom"
+import M from "materialize-css"
 
 const SignIn = () => {
+  const history = useHistory();
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const postData = () => {
+   if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) {
+
+       M.toast({html : "Invalid Email",classes : "#c62828 red darken-3"})
+       return;
+   }
+   fetch("http://localhost:5000/signin",{
+     method : "post",
+     headers : {
+       "Content-Type" : "application/json"
+     },
+     body : JSON.stringify({
+       email,
+       password
+     })
+   }).then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if(data.error) {
+          M.toast({
+            html : data.error,classes : "#c62828 red darken-3"
+          })
+        }
+        else {
+          localStorage.setItem("jwt",data.token);
+          localStorage.setItem("user",JSON.stringify(data.user));
+          M.toast({
+            html : "User signed in successfully",classes : "#4caf50 green"
+          })
+          history.push("/");
+        }
+      })
+    }
+
     return (
         <div className="mycard">
           <div className="card auth-card">
             <h2> Instagram</h2>
-
-            <input type ="text" placeholder="email" />
-            <input type ="text" placeholder="password" />
-            <button className="btn waves-effect waves-light">Submit
-                Login
+            <input type ="text" value={email} onChange={(e) => {setEmail(e.target.value)}} placeholder="email" />
+            <input type ="text" value={password} onChange={(e) => {setPassword(e.target.value)}} placeholder="password" />
+            <button className="btn waves-effect waves-light" onClick ={() => postData()}>Submit
+                Sign In
             </button>
-            {/* <Router>
-                <Link to = "/signup"> Don't have an account?</Link>
-            </Router> */}
+            <br /><br />
+                <Link to = "/signup"> Don't have an account? Sign Up</Link>
           </div>
         </div>
 
--- client/src/screens/SignUp.js
+++ client/src/screens/SignUp.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react"
 import {Link,useHistory} from "react-router-dom"
-import axios from "axios"
 import M from "materialize-css"
+
 const SignUp = () => {
   const history = useHistory();
   const [name,setName] = useState("");
@@ -9,7 +9,7 @@ const SignUp = () => {
   const [password,setPassword] = useState("");
 
   const postData = () => {
-    if(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     ){
        M.toast({html : "Invalid Email",classes : "#c62828 red darken-3"})
        return;
@@ -25,22 +25,22 @@ const SignUp = () => {
        password
      })
    }).then((res) => res.json())
-   .then((data) => {
-     console.log(data.error);
-     if(data.error) {
-       M.toast({
-        html : data.error,classes : "#c62828 red darken-3"
-       })
-     }
-     else {
-       M.toast({
-         html : data.message,classes : "#4caf50 green"
-       })
-       history.push("/signin");
-     }
-   })
-   console.log(name,email,password);
-}
+      .then((data) => {
+        console.log(data.error);
+        if(data.error) {
+          M.toast({
+            html : data.error,classes : "#c62828 red darken-3"
+          })
+        }
+        else {
+          M.toast({
+            html : data.message,classes : "#4caf50 green"
+          })
+          history.push("/signin");
+        }
+      })
+      console.log(name,email,password);
+    }
 
     return (
         <div className="mycard">
@@ -52,10 +52,10 @@ const SignUp = () => {
             <input type ="text" value={password} onChange={(e) => {setPassword(e.target.value)}} placeholder="password" />
 
             <button className="btn waves-effect waves-light" onClick ={() => postData()}>
-                SignUp
+                Sign Up
             </button>
             <br /> <br />
-                <Link to = "/signin"> Already have an account?</Link>
+                <Link to = "/signin"> Already have an account? Sign In</Link>
           </div>
         </div>
 
