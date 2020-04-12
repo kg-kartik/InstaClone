6f3cdf200e75012ef58b01ec7f01771e3dec51d0
Kartik Goel <goel.kartik39@gmail.com>
2020-04-13 02:17:49 +0530

    Added links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,11 +5,12 @@ const Navbar = () => {
     return (
     <nav>
         <div className="nav-wrapper white">
-            <a href="#" className="brand-logo left">Instagram</a>
+            <a href="/" className="brand-logo left">Instagram</a>
             <ul id="nav-mobile" className="right">
             <li><a href="/signup">SignUp</a></li>
             <li><a href="/signin">SignIn</a></li>
             <li><a href="/profile">Profile</a></li>
+            <li><a href="/createpost">CreatePost</a></li>
             </ul>
         </div>
     </nav>
