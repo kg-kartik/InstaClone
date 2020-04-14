b6958645a4133b3d0f62c6600c3033c5ace10851
Kartik Goel <goel.kartik39@gmail.com>
2020-04-14 22:58:13 +0530

    made allpost protected route

--- routes/post.js
+++ routes/post.js
@@ -4,7 +4,7 @@ const Post = require("../models/post");
 const requireLogin = require("../middleware/requireLogin");
 
 //To get all posts from all users
-router.get('/allpost',(req,res) => {
+router.get('/allpost',requireLogin,(req,res) => {
     Post.find()
     .populate("postedBy","_id name")
     .then((posts) => {
