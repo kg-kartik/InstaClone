de4832e73a7e6a86f48ccc611405efe1481a1983
Kartik Goel <goel.kartik39@gmail.com>
2020-04-14 22:57:19 +0530

    Fetched posts from db

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,22 +1,42 @@
-import React from "react"
+import React,{useState, useEffect} from "react"
 import image from "../wallpaper.jpg"
 
 const Home = () => {
+    const [data,setData] = useState([]);
+    useEffect(() =>{
+        fetch('/allpost',{
+            headers : {
+                "Authorization" : "Bearer " + localStorage.getItem("jwt")
+            }
+        }).then((res) => res.json())
+        .then((result) => {
+            console.log(result);
+            setData(result);
+        })
+    },[])
     return (
-        <div className="home">
+        <div className = "home">
+       {
+           data.map((item)=>{
+            return (
+            <div className="home">
             <div className="card home-card">
-                <h5> Kartik </h5>
+                <h5> {item.postedBy.name} </h5>
                 <div className="card-image">
-                    <img src={image} />
+                    <img src={item.photo} />
                 </div>
                 <div className="card-content">
-                    <h6> Title</h6>
-                    <p> An amazing post</p>
+                    <h6> {item.title}</h6>
+                    <p> {item.body}</p>
                     <input type="text" placeholder="Add your comment" />
                 </div>
             </div>
 
         </div>
+            ) })
+       }
+       </div>
+        
     )
 }
 
