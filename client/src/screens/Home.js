fe83a18a6f9b9b5ededa2735bef21aed509f956b
Kartik Goel <goel.kartik39@gmail.com>
2020-04-15 16:56:07 +0530

    Implemented like and unlike in react

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,8 +1,9 @@
-import React,{useState, useEffect} from "react"
-import image from "../wallpaper.jpg"
+import React,{useState, useEffect,useContext} from "react"
+import {UserContext} from "../App"
 
 const Home = () => {
     const [data,setData] = useState([]);
+    const {state,dispatch} = useContext(UserContext)
     useEffect(() =>{
         fetch('/allpost',{
             headers : {
@@ -10,10 +11,69 @@ const Home = () => {
             }
         }).then((res) => res.json())
         .then((result) => {
-            console.log(result);
             setData(result);
         })
     },[])
+
+    //Like posts
+    const likePost = (id) => {
+        fetch('http://localhost:5000/likes',{
+            method : "post",
+            headers : {
+                "Authorization" : "Bearer " + localStorage.getItem("jwt"),
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify({
+                postId : id 
+            })
+        }).then((res) => res.json())
+        .then((result) => {
+
+            //Updating the state with likes array
+            const newData = data.map((item) => {
+                if(item._id == result._id){
+                    return result
+                }
+                else {
+                    return item
+                }
+            })
+            setData(newData);
+
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
+    //Unlike Posts 
+    const unlikePost = (id) => {
+        fetch('http://localhost:5000/unlikes',{
+            method : "post",
+            headers : {
+                "Authorization" : "Bearer " + localStorage.getItem("jwt"),
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify({
+                postId : id 
+            })
+        }).then((res) => res.json())
+        .then((result) => {
+
+            const newData = data.map((item) => {
+                if(item._id == result._id){
+                    return result
+                }
+                else {
+                    return item
+                }
+            })
+            setData(newData);
+
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     return (
         <div className = "home">
        {
@@ -25,7 +85,16 @@ const Home = () => {
                 <div className="card-image">
                     <img src={item.photo} />
                 </div>
+
                 <div className="card-content">
+                    {item.likes.includes(state._id) ? 
+                        <i className="material-icons" 
+                        onClick = {() => { unlikePost(item._id) }}>thumb_down</i>
+                     :
+                        <i className="material-icons"
+                        onClick = {() => { likePost(item._id) }}>thumb_up</i>
+                }
+                    <h6> {item.likes.length} Likes</h6>
                     <h6> {item.title}</h6>
                     <p> {item.body}</p>
                     <input type="text" placeholder="Add your comment" />
