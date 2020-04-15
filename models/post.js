d1a3590d2a5d9e5b42df2fbd2eb9dc14a1b40816
Kartik Goel <goel.kartik39@gmail.com>
2020-04-15 16:58:56 +0530

    Added like property

--- models/post.js
+++ models/post.js
@@ -16,6 +16,12 @@ const postSchema = new Schema({
         type : String,
         required : true
     },
+    likes : [
+        {
+            type : ObjectId,
+            ref : "User"
+        }
+    ],
     postedBy : {
         type :ObjectId,
         ref : "userModel"
