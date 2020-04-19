b8711818acfb6eeafe9bf256c3650388de992455
Kartik Goel <goel.kartik39@gmail.com>
2020-04-19 12:27:20 +0530

    Added follow and unfollow routes

--- routes/user.js
+++ routes/user.js
@@ -19,8 +19,53 @@ router.get("/user/:id",requireLogin,(req,res) => {
             res.json({user,post});
         })
     }).catch((err) => {
-        res.json(err);
+        console.log(err);
+    }).catch((err) => {
+        console.log(err);
     })
 })
 
+//Follow route
+router.post('/follow',requireLogin,(req,res)=>{
+    User.findByIdAndUpdate(req.body.followId,{
+        $push:{followers:req.user._id}
+    },{
+        new:true
+    },(err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+      User.findByIdAndUpdate(req.user._id,{
+          $push:{following:req.body.followId}
+          
+      },{new:true}).select("-password").then(result=>{
+          res.json(result)
+      }).catch(err=>{
+          return res.status(422).json({error:err})
+      })
+    })
+})
+
+//Unfollow route
+router.post('/unfollow',requireLogin,(req,res)=>{
+    User.findByIdAndUpdate(req.body.unfollowId,{
+        $pull:{followers:req.user._id}
+    },{
+        new:true
+    },(err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }
+      User.findByIdAndUpdate(req.user._id,{
+          $pull:{following:req.body.unfollowId}
+          
+      },{new:true}).select("-password").then(result=>{
+          res.json(result)
+      }).catch(err=>{
+          return res.status(422).json({error:err})
+      })
+
+    }
+    )
+})
 module.exports = router;
